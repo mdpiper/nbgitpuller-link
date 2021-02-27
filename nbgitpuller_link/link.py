ef16c4f103cd5356996d954f8d3f6e00c9f44882
Mark Piper <mark.piper@colorado.edu>
2021-02-27 15:30:38 -0700

    Check validity of input URLs

--- nbgitpuller_link/link.py
+++ nbgitpuller_link/link.py
@@ -12,20 +12,21 @@ class Link:
         self, jupyterhub_url, repository_url, branch=None, launch_path=None
     ):
 
-        self._jupyterhub = urllib.parse.urlsplit(jupyterhub_url)
-        self._repository = repository_url
+        # self._jupyterhub = urllib.parse.urlsplit(jupyterhub_url)
+        self._jupyterhub = self._validate_link(jupyterhub_url)
+        self._repository = self._validate_link(repository_url)
         self._branch = branch or "main"
         self._launch = launch_path or ""
 
         self._generate_link()
 
-        valid = validators.url(self.link)
-        if not valid:
-            raise ValueError("Invalid link: {}".format(valid))
+        # valid = validators.url(self.link)
+        # if not valid:
+        #     raise ValueError("Invalid link: {}".format(valid))
 
     @property
     def link(self):
-        return self._link
+        return self._validate_link(self._link)
 
     def _generate_query(self):
         repo_name = urllib.parse.urlsplit(self._repository).path.split("/")[-1]
@@ -42,12 +43,19 @@ class Link:
         return query
 
     def _generate_link(self):
+        jupyterhub = urllib.parse.urlsplit(self._jupyterhub)
         self._link = urllib.parse.urlunsplit(
             (
-                self._jupyterhub.scheme,
-                self._jupyterhub.netloc,
+                jupyterhub.scheme,
+                jupyterhub.netloc,
                 "hub/user-redirect/git-pull",
                 self._generate_query(),
                 "",
             )
         )
+
+    def _validate_link(self, link):
+        valid = validators.url(link)
+        if not valid:
+            raise ValueError("Invalid link: {}".format(valid))
+        return link

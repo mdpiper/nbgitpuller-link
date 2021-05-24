dddb83e22150b9d936e6452ca12e2a0d3422da03
Mark Piper <mark.piper@colorado.edu>
2021-05-24 16:33:30 -0600

    Handle spaces in path

--- examples/nbgitpuller_link_ex.sh
+++ examples/nbgitpuller_link_ex.sh
@@ -4,7 +4,7 @@
 HUB=https://csdms.rc.colorado.edu
 REPO=https://github.com/csdms/espin
 BRANCH=main
-FILE=lessons/jupyter/index.ipynb
+FILE=lessons/jupyter/index.ipynb  # Note: escape space in path with backslash
 
 nbgitpuller-link --version
 nbgitpuller-link --help
@@ -13,4 +13,4 @@ nbgitpuller-link \
     --jupyterhub-url=$HUB \
     --repository-url=$REPO \
     --branch=$BRANCH \
-    --launch-path=$FILE
+    --launch-path="$FILE"

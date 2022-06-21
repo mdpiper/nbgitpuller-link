3175dd0cc9d4fe2508a37b64b1b681d07500b1fa
Mark Piper <mark.piper@colorado.edu>
2022-06-21 16:11:06 -0600

    Make pretty

--- examples/nbgitpuller_link_ex.py
+++ examples/nbgitpuller_link_ex.py
@@ -1,11 +1,11 @@
 """Example of constructing an nbgitpuller link."""
 from nbgitpuller_link import Link
 
-HUB="https://csdms.rc.colorado.edu"
-REPO="https://github.com/csdms/espin"
-BRANCH="main"
-FILE="lessons/jupyter/index.ipynb"
-INTERFACE="lab"
+HUB = "https://csdms.rc.colorado.edu"
+REPO = "https://github.com/csdms/espin"
+BRANCH = "main"
+FILE = "lessons/jupyter/index.ipynb"
+INTERFACE = "lab"
 
 
 linker = Link(
@@ -14,6 +14,6 @@ linker = Link(
     branch=BRANCH,
     launch_path=FILE,
     interface=INTERFACE,
-    )
+)
 
 print("The nbgitpuller link is:\n{}".format(linker.link))

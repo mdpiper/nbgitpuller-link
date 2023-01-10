f6c829a61b283a5ad4da7296fa44f0164dedd8c0
Mark Piper <mark.piper@colorado.edu>
2023-01-10 16:06:01 -0700

    Add test_cli, clean sessions to noxfile

---
+++ noxfile.py
@@ -0,0 +1,32 @@
+import pathlib
+import shutil
+from itertools import chain
+
+import nox
+
+PROJECT = "nbgitpuller-link"
+PACKAGE = "nbgitpuller_link"
+ROOT = pathlib.Path(__file__).parent
+
+
+@nox.session(python=False)
+def clean(session):
+    """Remove virtual environments, build files, and caches."""
+    shutil.rmtree("build", ignore_errors=True)
+    shutil.rmtree("wheelhouse", ignore_errors=True)
+    shutil.rmtree(f"{PACKAGE}.egg-info", ignore_errors=True)
+    shutil.rmtree(".pytest_cache", ignore_errors=True)
+    shutil.rmtree(".venv", ignore_errors=True)
+    for p in chain(ROOT.rglob("*.py[co]"), ROOT.rglob("__pycache__")):
+        if p.is_dir():
+            p.rmdir()
+        else:
+            p.unlink()
+
+
+@nox.session
+def test_cli(session: nox.Session) -> None:
+    """Test the command line interface."""
+    session.install(".")
+    session.run(PROJECT, "--version")
+    session.run(PROJECT, "--help")
